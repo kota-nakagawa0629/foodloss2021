c503e699ae244e10768f2d26692366ddcde6bbef
kota-nakagawa0629 <ayuri.ofuchi@hotmail.com>
2021-05-30 11:28:43 +0000

    for heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 255 }
-  validates :price, presence: true, numericality: {only_integer: true}, length: { maximum: 255 }
-  validates :stock, presence: true, numericality: {only_integer: true}, length: { maximum: 255 }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}, length: { maximum: 255 }
+  validates :stock, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}, length: { maximum: 255 }
   
   mount_uploader :photo, PhotoUploader
 
