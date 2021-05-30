c2271c4c5edfb0bc688e1fb23dbefdef0208ff7f
kota-nakagawa0629 <ayuri.ofuchi@hotmail.com>
2021-05-30 13:07:51 +0000

    for heroku

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,14 +2,14 @@ class TradesController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     current_user.trade(item)
-    flash[:success] = 'お気に入りに追加しました。'
+    flash[:success] = '購入希望に追加しました。'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     item = Item.find(params[:item_id])
     current_user.untrade(item)
-    flash[:success] = 'お気に入りの追加を解除しました。'
+    flash[:success] = '購入希望を削除しました。'
     redirect_back(fallback_location: root_path)
   end
   
